a01f9204b4042a22c332928d62f3093a5eb0d4cf
devWhisper <tsimpos@agrotechniki.gr>
2019-02-13 18:02:08 +0200

    test commit

---
+++ lampIR.ino
@@ -0,0 +1,20 @@
+//Buttons cookbook
+//A --> on/off lamp power
+//B --> enable/disable photoresistor
+//C --> enable/disable IR
+const int buttonAPin = 2;
+const int buttonBPin = 3;
+const int buttonCPin = 4;
+
+
+int buttonState = 0;         // variable for reading the pushbutton status
+
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+}
